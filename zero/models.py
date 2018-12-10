8c449d22a79828ca91491a011e909d3bd33bce0c
pangzi499 <pangzi499@gmail.com>
2018-12-10 11:32:07 +0800

    Rescan

---
+++ zero/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class zeroHistory(models.Model):#数据库创建
+    xTarget = models.CharField('Target',max_length=30)
+    xType = models.CharField('Type',max_length=30)
+    xInfo = models.CharField('Info',max_length=10)
+
+    class Meta:
+        db_table = 'zeroHistory'
